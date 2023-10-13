3f1ec6b1d7f3bbd11228e21459363d127feebab2
Sakshi Ojha <127045654+Sakshi-2707@users.noreply.github.com>
2023-10-13 18:38:02 +0530

    Create Celsius_to_fahrenheit.java

---
+++ Celsius_to_fahrenheit.java
@@ -0,0 +1,10 @@
+class Celsius_to_fahrenheit {
+    public static void main(String[] args)
+    {
+        double celsius = 10.0, fahrenheit = 0.0;
+        fahrenheit = (celsius * 1.8) + 32;
+        System.out.println(
+            " value of temperature in fahrenheit:"
+            + fahrenheit);
+    }
+}
